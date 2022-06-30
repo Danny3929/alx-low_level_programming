43c5a312119c2486c54ac039dcab4ad09e4b13de
Danny3929 <danielmwenda3929@gmail.com>
2022-06-30 14:44:38 +0000

    l

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Do well
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("Size of a char: %d\n byte(s)");
+	printf("Size of an int: %d\n byte(s)");
+	printf("Size of a long int: %d\n byte(s)");
+	printf("Size of a long long int: %d\n byte(s)");
+	printf("Size of a float: %d\n byte(s)");
+	return(0);
+
