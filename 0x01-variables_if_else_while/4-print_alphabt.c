1407e173535a22dc7e639fd02e276c318476b168
Danny3929 <danielmwenda3929@gmail.com>
2022-07-01 19:47:16 +0000

    l

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - how are you
+ *
+ * Return: ALways 0 (success)
+ */
+
+int main(void)
+{
+	int ch
+	for ( ch = 'a' ; ch <= 'z' ; ch++);
+	{
+		if (ch != 'e' && ch != 'q');
+			putchar(ch);
+	}
+	putchar('\n');
+
+	return (0);
+
+}
