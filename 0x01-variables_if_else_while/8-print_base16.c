080f1ad8b62ada284f4a35326b68eaf93b6bbc7e
Danny3929 <danielmwenda3929@gmail.com>
2022-07-01 21:20:45 +0000

    l

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -8,9 +8,14 @@
 int main(void)
 {
 	int i;
+	char t;
 
-	for (i = 0 ; i <= 16 ; i++)
-		putchar(i);
+	for (i = 0 ; i < 10 ; i++)
+		putchar((i % 10) + '0');
+	for (t = 'a' ; t <= 'f' ; t++)
+		putchar(t);
+
 	putchar('\n');
+
 	return (0);
 }
