cd671007b1d337d06031cfe8de78db9b800eec75
Danny3929 <danielmwenda3929@gmail.com>
2022-07-01 19:55:19 +0000

    l

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,11 +9,12 @@ int main(void)
 {
 	int ch;
 
-	for ( ch = 'a' ; ch <= 'z' ; ch++);
+	for (ch = 'a' ; ch <= 'z' ; ch++);
 	{
 		if (ch != 'e' && ch != 'q');
-			putchar(ch);
+		putchar(ch)
 	}
+
 	putchar('\n');
 
 	return (0);
