3b39b30233e2339d25c1163c9715a1be7e5e23f8
Danny3929 <danielmwenda3929@gmail.com>
2022-07-05 12:11:45 +0000

    l

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -12,7 +12,7 @@ void print_alphabet_x10(void)
 
 	for (i = 0 ; i <= 9 ; i++)
 	{
-		for (alphabets = 97 ; alphabets <= 122 ; b++)
+		for (alphabets = 97 ; alphabets <= 122 ; alphabets++)
 		{
 			putchar(alphabets);
 		}
