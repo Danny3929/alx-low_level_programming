2039e0b6ddfd0ef7cc553278ec70eb75bee25e05
Danny3929 <danielmwenda3929@gmail.com>
2022-07-05 12:10:42 +0000

    l

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
+{
+	int alphabets, i;
+
+	for (i = 0 ; i <= 9 ; i++)
+	{
+		for (alphabets = 97 ; alphabets <= 122 ; b++)
+		{
+			putchar(alphabets);
+		}
+		putchar('\n');
+	}
+}
