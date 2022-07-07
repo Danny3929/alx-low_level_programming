eea2eb8d8c12b5c6a917371ef56037ddd9414bb8
Danny3929 <danielmwenda3929@gmail.com>
2022-07-07 10:54:21 +0000

    l

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -2,7 +2,7 @@
 #include "main.h"
 
 /**
- * print_most_numbers - prints the numbers,from 0-9
+ * print_most_numbers - prints the numbers,from
  * @void: integer
  * Return: Always 0
  */
@@ -11,12 +11,10 @@ void print_most _numbers(void)
 {
 	int c;
 
-	for (c = 48 ; c <= 57 ; c++)
+	for (c = 48 ; c < 58 ; c++)
 	{
 		if (c != 50 && c != 52)
-		{
 			_putchar(c);
-		}
 	}
 	_putchar('\n');
 }
