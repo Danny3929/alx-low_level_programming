361e74cf976831b1965b85863896ff5dbf9371e1
Danny3929 <danielmwenda3929@gmail.com>
2022-07-07 18:59:27 +0000

    l

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,32 +9,31 @@
 
 int main(void)
 {
-	int i;
-
-	i = 1;
-	printf("%d", i);
-
-	for (i = 2 ; i <= 100 ; i++)
+	int n;
+	
+	n = 1;
+	printf("%d", n);
+	
+	for (n = 2; n <= 100; n++)
 	{
-		if ((i % 3 == 0) && (i % 5 == 0))
+		if ((n % 3 == 0) && (n % 5 == 0))
 		{
-			printf("FizzBuzz");
+			printf(" FizzBuzz");
 		}
-		else if (i % 3 == 0)
-		{
-			printf("Fizz");
-		}
-		else if (i % 5 == 0)
-		{
-			printf("Buzz");
-		}
-		else
-		{
-			printf("%d", i);
-		}
+	
+	else if (n % 3 == 0)
+	{
+		printf(" Fizz");
+	}
+	else if (n % 5 == 0)
+	{
+		printf(" Buzz");
+	}
+	else
+	{
+		printf(" %d", n);
+	}
 	}
 	printf("\n");
-
 	return (0);
 }
-
