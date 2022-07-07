3d4a4013826c19cb64cd3967fedb0f261623190f
Danny3929 <danielmwenda3929@gmail.com>
2022-07-07 12:39:04 +0000

    l

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -11,7 +11,7 @@ void print_diagonal(int n)
 {
 	int a, b;
 
-	for(a = 0 ; a < n ; a++)
+	for (a = 0 ; a < n ; a++)
 	{
 		for (b = 0 ; b < a ; b++)
 		{
@@ -19,7 +19,7 @@ void print_diagonal(int n)
 		}
 		_putchar(92);
 	if (a < (n - 1))
-	
+
 		_putchar('\n');
 	}
 	_putchar('\n');
