39c0a360bdb74d9cf6f06ae726f02a81b3b64329
Danny3929 <danielmwenda3929@gmail.com>
2022-07-11 13:29:09 +0000

    l

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _puts - function that prints a string, followed by a new line
+ * @str: character
+ * Return: Always
+ */
+
+void _puts(char *str)
+{
+	printf("%s\n", *str);
+	return ('0');
+}
