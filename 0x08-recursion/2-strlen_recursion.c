330bbb2e243dc10462122014062607d007408e17
Danny3929 <danielmwenda3929@gmail.com>
2022-07-20 10:21:32 +0000

    l

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -11,9 +11,7 @@ int _strlen_recursion(char *s)
 {
 	if (*s != '\0')
 	{
-		_strlen_recursion(s);
-		s++;
-		return(int _strlen_recursion);
+		return (1 + _strlen_recursions(s+1)
 	}
 	return(0);
 }
