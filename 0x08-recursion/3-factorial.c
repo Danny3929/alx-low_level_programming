1dd5a2accda7aba107bc95f324932535cb8ea0bc
Danny3929 <danielmwenda3929@gmail.com>
2022-07-20 10:49:39 +0000

    l

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -10,12 +10,14 @@
 
 int factorial(int n)
 {
-	int factorial'0' = 1;
-
-	if (n >= 0)
+	if (n = 0)
 	{
-		return (factorial (n));
+		return (1);
+	}
+	else if (n > 0)
+	{
+		return (n * factorial(n - 1));
 	}
 	else
-		return (-1);
+		return(-1);
 }
