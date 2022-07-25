8110f64d931ef2fb83d072264c2a26dd4c8075d5
Danny3929 <danielmwenda3929@gmail.com>
2022-07-25 14:48:12 +0000

    l

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,6 +1,4 @@
 #include "main.h"
-#include <stdlib.h>
-#include <stdio.h>
 
 /**
  * _strlen - length of a string
@@ -32,33 +30,26 @@ char *str_concat(char *s1, char *s2)
 	unsigned int l1, l2;
 	char *conc, *tmp;
 
-	printf("checking:%s", s1);
-
-	if (s1 == NULL)
-		s1 = " ";
+	if (!s1)
+		s1 = "";
 	else
 		l1 = _strlen(s1);
-	printf("checking:%s\n ", s1);
-	printf("checking:%ld\n", sizeof s1);
 
-	if (s2 == NULL)
-		s2 = " ";
+	if (!s2)
+		s2 = "";
 	else
 		l2 = _strlen(s2);
-	printf("checking:%s\n", s2);
 
 	conc = malloc(l1 + l2 + 1);
-	if (conc == NULL)
+	if (!conc)
 		return (0);
 
 	tmp = conc;
 	while (*s1)
 		*tmp++ = *s1++;
-	printf("checking:%ld\n", sizeof s1);
-	while ((*tmp++ = *s2++))
-	;
 
-	printf("tmp:%c ",*tmp);
+	while ((*tmp++ = *s2++))
+		;
 
 	return (conc);
 }
