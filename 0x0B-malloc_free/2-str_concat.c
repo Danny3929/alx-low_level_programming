e59d0f6bb86209ffd396e2e2612b5254f6fc9238
Danny3929 <danielmwenda3929@gmail.com>
2022-07-25 14:31:58 +0000

    l

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,28 +1,30 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
 
 /**
- * _strlen - counts the length of the string
- * @s: string to be counted
- * Return: the length of string
+ * _strlen - length of a string
+ * @s: input char
+ * Return: length of a string
  */
 
 int _strlen(char *s)
 {
-	unsigned int l = 0;
+	int l = 0;
 
 	while (*s != '\0')
+	{
 		s++;
-	l++;
+		l++;
+	}
 	return (l);
 }
 
 /**
- * str_concat - funtion that concatenates 2 strings
- * @s1: pointer to the first string
- * @s2: pointer to the second string
- * Return: ' ' if NULL is passed and return NULL on failure
+ * str_concat - Concat 2 strings.
+ * @s1: string
+ * @s2: string
+ * Return: char
  */
 
 char *str_concat(char *s1, char *s2)
@@ -37,13 +39,13 @@ char *str_concat(char *s1, char *s2)
 	else
 		l1 = _strlen(s1);
 	printf("checking:%s\n ", s1);
-	printf("checking:%ld\n", sizeof(s1));
+	printf("checking:%ld\n", sizeof s1);
 
 	if (s2 == NULL)
 		s2 = " ";
 	else
 		l2 = _strlen(s2);
-	printf("checking:%s\n",s2);
+	printf("checking:%s\n", s2);
 
 	conc = malloc(l1 + l2 + 1);
 	if (conc == NULL)
@@ -52,11 +54,11 @@ char *str_concat(char *s1, char *s2)
 	tmp = conc;
 	while (*s1)
 		*tmp++ = *s1++;
-	printf("checking:%ld\n", sizeof(s1));
+	printf("checking:%ld\n", sizeof s1);
 	while ((*tmp++ = *s2++))
 	;
 
-	printf("tmp:%c ", *tmp);
+	printf("tmp:%c ",*tmp);
 
 	return (conc);
 }
